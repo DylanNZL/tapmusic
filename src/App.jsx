9c62001495b511bd0adcaf61dd10e2ed5529fd40
dylancross <git@dylancross.nz>
2022-01-29 12:14:35 +1300

    Finish settings form

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,25 @@ import { Col, Container, Form, Row } from "react-bootstrap";
 
 function App() {
   // States
+  const [timeSigBeat, setTimeSigBeat] = useState(4);
+  const [timeSigNote, setTimeSigNote] = useState(4);
+  const [preferredNote, setPreferredNote] = useState("quaver");
   const [tempo, setTempo] = useState(120);
   const [tempos] = useState([Date.now(), Date.now(), Date.now(), Date.now()]);
 
+  // tapTempo allows the user to modify the tempo by tapping the tempo out with mouse clicks instead.
   //
+  // The logic for this tap tempo implementation looks at when the user clicked 3 taps ago and the latest tap. If that
+  //  value is more than 4500ms we skip the tap tempo update as that would result in too low of a BPM. We then take that
+  //  difference and convert it to seconds and use that to divide 3 as that is how many differences between beats we have.
   function tapTempo() {
+    console.log(timeSigBeat, timeSigNote, preferredNote);
+
     tempos.shift();
     tempos.push(Date.now());
 
     const difference = tempos.at(3) - tempos.at(0);
-    if (difference < 4500) {
+    if (difference > 4500) {
       console.log(
         `Skipping tap update as the bpm would be too low: ${difference}`
       );
@@ -36,7 +45,8 @@ function App() {
               <Form.Label>Time Signature</Form.Label>
               <Form.Select
                 aria-label="Select for the beat part of the Time Signature"
-                defaultValue="4"
+                value={timeSigBeat}
+                onChange={(e) => setTimeSigBeat(e.target.value)}
               >
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -49,7 +59,8 @@ function App() {
               </Form.Select>
               <Form.Select
                 aria-label="4 for the note part of the Time Signature"
-                defaultValue="quaver"
+                defaultValue={timeSigNote}
+                onChange={(e) => setTimeSigNote(e.target.value)}
               >
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -64,7 +75,8 @@ function App() {
               <Form.Label>Preferred Note</Form.Label>
               <Form.Select
                 aria-label="Select for the preferred note"
-                defaultValue="quaver"
+                defaultValue={preferredNote}
+                onChange={(e) => setPreferredNote(e.target.value)}
               >
                 <option value="breve">Breve</option>
                 <option value="semibreve">Semibreve</option>
